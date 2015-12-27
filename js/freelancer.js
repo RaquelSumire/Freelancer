446ac020c8241472257175c64b90d7082f02225d
Raquel Sumire <sumireraquel@gmail.com>
2015-12-26 23:44:25 -0500

    Modificaciones3

--- js/freelancer.js
+++ js/freelancer.js
@@ -2,3 +2,15 @@
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+	$(document).ready(init);
+	function init()
+	{
+		//inicio de aplicación
+		$(".main-menu ul li a").click(selectMenu);	
+	};
+	
+	function selectMenu()
+	{
+	$(".main-menu ul li a").removeClass("active");
+	$(this).addClass("active");
+    };
